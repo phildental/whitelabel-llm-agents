8192c988e9b6657481d25dc58e41a1ba0d323ccb
Felipe Marques <45220367+phildental@users.noreply.github.com>
2023-10-15 15:39:40 -0700

    Update personal-finance.py

--- personal-finance.py
+++ personal-finance.py
@@ -3,12 +3,48 @@ import requests
 import streamlit as st
 import os
 import pandasai
-from pandasai.responses.response_parser import ResponseParser
 from pandasai.llm import OpenAI
 from pandasai import SmartDataframe
 from pandasai.helpers.openai_info import get_openai_callback
 from dotenv import load_dotenv
 
+
+
+class StreamlitResponse(ResponseParser):
+    def __init__(self, context):
+        super().__init__(context)
+
+    def format_plot(self, result) -> None:
+        """
+        Display plot against a user query in Streamlit
+        Args:
+            result (dict): result contains type and value
+        """
+        import matplotlib.pyplot as plt
+        import matplotlib.image as mpimg
+
+        # Load the image file
+        try:
+            image = mpimg.imread(result["value"])
+        except FileNotFoundError:
+            raise FileNotFoundError(f"The file {result['value']} does not exist.")
+        except OSError:
+            raise ValueError(f"The file {result['value']} is not a valid image file.")
+
+        try:
+            import streamlit as st
+        except ImportError:
+            raise ImportError(
+                "The 'streamlit' module is required to use StreamLit Response. "
+                "Please install it using pip: pip install streamlit"
+            )
+
+        # Display the image
+        plt.imshow(image)
+        fig = plt.gcf()
+        st.pyplot(fig)
+
+from pandasai.responses.response_parser import ResponseParser
 
 load_dotenv()
 GS_API = os.getenv('GS_API')
