55767113979c7aac5fd323645eb13fa95a574c5e
Felipe Marques <45220367+phildental@users.noreply.github.com>
2023-10-15 14:00:15 -0700

    Update personal-finance.py

--- personal-finance.py
+++ personal-finance.py
@@ -3,6 +3,7 @@ from pandasai.llm import OpenAI
 from pandasai import SmartDataframe
 import requests
 import streamlit as st
+from pandasai.responses.streamlit_response import StreamlitResponse
 from pandasai.helpers.openai_info import get_openai_callback
 import os
 from dotenv import load_dotenv
@@ -27,15 +28,15 @@ df = dfnor[dfnor['hash'] != ""]
 
 def main():
     llm = OpenAI(api_token=OPENAI_API_KEY, temperature=0)
-    sdf = SmartDataframe(df, config={"llm": llm, "verbose": True})
+    sdf = SmartDataframe(df, config={"llm": llm, "enable_cache": True, "verbose": True, "response_parser": StreamlitResponse, "max_retries": 10})
     st.set_page_config(
         page_title="You Personal Finance Assistant 🧞‍♂️",
         page_icon=":sales:",
         layout="centered"
         )
     
-    st.header("Hey Phil! Nice to have you back. 🧞‍♂️💰")
-    user_question = st.text_input("Do you want to ask me a question about your finances?")
+    st.header("Hi, I am PennyPal! 🧞‍♂️💰")
+    user_question = st.text_input("Ask me a question about your finances.")
     if user_question is not None and user_question != "":
         with get_openai_callback() as cb:
             output = sdf.chat(user_question)
