519f3fd1f9f474d5374f5622950e231c874f2bc2
Felipe Marques <45220367+phildental@users.noreply.github.com>
2024-11-04 17:15:37 +0000

    Changed size of embedding

--- personal-finance.py
+++ personal-finance.py
@@ -16,7 +16,7 @@ def main():
     user_question = st.text_input("Ask me anything about your personal finance.")
     centering_html = """
     <div style='display: flex; justify-content: center; align-items: center; height: 135vh;'>
-        <iframe title="Personal Finance Dash v1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=dcc85b78-ef16-448a-9dfb-a22ab410f2bd&autoAuth=true&ctid=6dd87273-5fb3-4e6b-ac1c-029d6a5df713" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="Personal Finance Dash v1" width="1920" height="1080" src="https://app.powerbi.com/reportEmbed?reportId=dcc85b78-ef16-448a-9dfb-a22ab410f2bd&autoAuth=true&ctid=6dd87273-5fb3-4e6b-ac1c-029d6a5df713" frameborder="0" allowFullScreen="true"></iframe>
     </div>
     """
     st.markdown(centering_html, unsafe_allow_html=True)
